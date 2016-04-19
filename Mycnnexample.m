52c66eeb4fd3660c8aa0a2044473a4a2ce4cb64b
CuiHe <c253406143@163.com>
2016-04-19 21:31:12 +0800

    下载了据说是微软的RGBD图，但是不会载入啊

--- Mycnnexample.m
+++ Mycnnexample.m
@@ -3,18 +3,18 @@ clc;clear;
 t = tic;
 fprintf('\n现在读入数据...');
 
-
 neg = 112; %没有目标的图像
 pos = 112; %有目标的图像
-[test_x, test_y] = GDInit('D:\!zju\！Graduation design\image\s\small_48\', [neg,pos]); 
+[test_x, test_y] = GDInit('D:\!zju\！Graduation design\image\s\small_48\', [neg,pos]);
 
-%{
-neg = 19;
-pos = 21;
-[test_x, test_y] = GDInit('D:\!zju\！Graduation design\image\cut250\', [neg,pos]);
-%}
-train_x = test_x(:,:, [1:100, neg+1:neg+100]); % 这里选择全部数据都训练，一般可以留一部分用作测试
-train_y = test_y(:, [1:100, neg+1:neg+100]);
+temp_train_list = [1:14, neg+1:14]; % 这里选择训练数据 14个无 14个有
+train_x = test_x(:,:, temp_train_list); 
+train_y = test_y(:, temp_train_list);
+
+temp_test_list = [15:112, neg+15:112]; % 这里选择测试数据，剔除训练数据
+test_x = test_x(:,:, temp_test_list); 
+test_y = test_y(:, temp_test_list);
+
 time_input = toc(t);
 fprintf(' 完成 耗时：%.2f s', time_input);
 
@@ -37,8 +37,9 @@ fprintf(' 完成 耗时：%.2f s', time_build);
 t = tic;
 fprintf('\n现在训练网络... ');
 opts.alpha = 1;
-opts.batchsize = 40; %将所有数据打乱后 分size(train_x,3) / opts.batchsize批放入cnn（每个数据训练一次）
-opts.numepochs = 100; %上述操作做opts.numepochs次（每个数据训练100次）
+opts.batchsize = 14; %将所有数据打乱后 分size(train_x,3) / opts.batchsize批放入cnn（每个数据训练一次）
+opts.numepochs = 3000; %上述操作做opts.numepochs次（每个数据训练opts.numepochs次）
+opts.error_limit = 0.001; %指定误差在区间 (opts.batchsize/(pos+neg), 0) 是有意义的
 fprintf('小批数=%d 大批数epoch=%d \n', size(train_x,3) / opts.batchsize, opts.numepochs);
 cnn = cnntrain(cnn, train_x, train_y, opts);
 time_train = toc(t);
